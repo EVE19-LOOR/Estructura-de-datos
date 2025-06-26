307f6986257a041fb090297d661571fc3dfc3bac
EVELIN LOOR <es.loorv@uea.edu.ec>
2025-06-26 08:10:29 -0500

    Limpieza: elimino obj del control de versiones

--- TareaSemanaCinco/Ejercicio1/obj/Debug/net9.0/Ejercio1.AssemblyInfo.cs
+++ TareaSemanaCinco/Ejercicio1/obj/Debug/net9.0/Ejercio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15718fb5178475d06d41d6e0faa1c70e026687b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb185433d444a2220547395a9f2a7ec5328edbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
