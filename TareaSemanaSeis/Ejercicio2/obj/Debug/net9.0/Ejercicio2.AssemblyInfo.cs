842f6457d7048c7b14e31cea30503f440f4ffd7d
EVELIN LOOR <es.loorv@uea.edu.ec>
2025-07-05 21:38:16 -0500

    Eliminar bin/ y obj/ del repositorio y agregar .gitignore

--- TareaSemanaSeis/Ejercicio2/obj/Debug/net9.0/Ejercicio2.AssemblyInfo.cs
+++ TareaSemanaSeis/Ejercicio2/obj/Debug/net9.0/Ejercicio2.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+052e330d8c6337ea9cca98790d96e0937cf61e56")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
