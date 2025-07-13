bc08bfe1e2bf50781102dd3463cc593a481c755f
EVELIN LOOR <es.loorv@uea.edu.ec>
2025-07-13 11:13:24 -0500

    TareaSemanaSiete

--- TareaSemanaSiete/Ejercicio1/Program.cs
+++ TareaSemanaSiete/Ejercicio1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class VerificarBalanceo
+{
+    static bool EstaBalanceada(string expresion)
+    {
+        Stack<char> pila = new Stack<char>();
+
+        foreach (char c in expresion)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                pila.Push(c);
+            }
+            else if (c == ')' || c == '}' || c == ']')
+            {
+                if (pila.Count == 0) return false;
+
+                char tope = pila.Pop();
+                if ((c == ')' && tope != '(') ||
+                    (c == '}' && tope != '{') ||
+                    (c == ']' && tope != '['))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return pila.Count == 0;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingresa una expresión matemática: ");
+        string expresion = Console.ReadLine();
+
+        if (EstaBalanceada(expresion))
+            Console.WriteLine("Fórmula balanceada.");
+        else
+            Console.WriteLine("Fórmula no balanceada.");
+    }
+}
--- TareaSemanaSiete/Ejercicio2/Program.cs
+++ TareaSemanaSiete/Ejercicio2/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class TorreDeHanoi
+{
+    static void MoverDiscos(int n, Stack<int> origen, Stack<int> destino, Stack<int> auxiliar, string nomOrigen, string nomDestino, string nomAuxiliar)
+    {
+        if (n == 1)
+        {
+            int disco = origen.Pop();
+            destino.Push(disco);
+            Console.WriteLine($"Mover disco {disco} de {nomOrigen} a {nomDestino}");
+        }
+        else
+        {
+            MoverDiscos(n - 1, origen, auxiliar, destino, nomOrigen, nomAuxiliar, nomDestino);
+            MoverDiscos(1, origen, destino, auxiliar, nomOrigen, nomDestino, nomAuxiliar);
+            MoverDiscos(n - 1, auxiliar, destino, origen, nomAuxiliar, nomDestino, nomOrigen);
+        }
+    }
+
+    static void Main()
+    {
+        int numDiscos = 3;
+        Stack<int> torreA = new Stack<int>();
+        Stack<int> torreB = new Stack<int>();
+        Stack<int> torreC = new Stack<int>();
+
+        for (int i = numDiscos; i >= 1; i--)
+        {
+            torreA.Push(i);
+        }
+
+        Console.WriteLine("Movimientos para resolver las Torres de Hanoi:");
+        MoverDiscos(numDiscos, torreA, torreC, torreB, "A", "C", "B");
+    }
+}
